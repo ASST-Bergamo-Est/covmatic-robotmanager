fb6716b151204456f8e3ae5062d3df92ec1732b2
Agostino Facotti <agostino.facotti@asst-bergamoest.it>
2023-01-27 19:02:25 +0100

    RobotManagerAPI changed manager import to avoid conflict on process spawn

--- src/covmatic_robotmanager/api.py
+++ src/covmatic_robotmanager/api.py
@@ -1,26 +1,32 @@
 import json
+import logging
 
 from flask import request
 from flask_restful import Api, Resource
 from . import __version__
 from .robot_manager import RobotManager
 
-robot_manager = RobotManager()
-
 
 class RobotManagerApi(Api):
     def __init__(self, *args, **kwargs):
         super(RobotManagerApi, self).__init__(*args, **kwargs)
-        self.add_resource(Version, '/version')
-        self.add_resource(CheckAction, '/action/check/<string:action_id>')
-        self.add_resource(RequestAction, '/action/<string:action>/<string:machine>/<string:slot>/<string:plate_name>')
+        self._robot_manager = RobotManager()
+        self.add_resource(Version, '/version', resource_class_args=(self._robot_manager,))
+        self.add_resource(CheckAction, '/action/check/<string:action_id>', resource_class_args=(self._robot_manager,))
+        self.add_resource(RequestAction, '/action/<string:action>/<string:machine>/<string:slot>/<string:plate_name>', resource_class_args=(self._robot_manager,))
 
-    @staticmethod
-    def shutdown():
-        robot_manager.shutdown()
+    def shutdown(self):
+        logging.getLogger().info("Api shutdown called")
+        self._robot_manager.shutdown()
 
 
-class Version(Resource):
+class RobotManagerResource(Resource):
+    def __init__(self, robot_manager):
+        super().__init__()
+        self._robot_manager = robot_manager
+
+
+class Version(RobotManagerResource):
     def get(self):
         return {
             'name': 'RobotManager server',
@@ -28,16 +34,16 @@ class Version(Resource):
         }
 
 
-class CheckAction(Resource):
+class CheckAction(RobotManagerResource):
     def get(self, action_id):
-        return robot_manager.check_action(action_id)
+        return self._robot_manager.check_action(action_id)
 
 
-class RequestAction(Resource):
+class RequestAction(RobotManagerResource):
     def post(self, action, machine, slot, plate_name):
         try:
             print("Received options: {}".format(request.get_json()))
         except TypeError as e:
             print("Got error: {}".format(e))
-        action_id = robot_manager.action_request(action, machine, slot, plate_name)
+        action_id = self._robot_manager.action_request(action, machine, slot, plate_name)
         return {'action_id': action_id}
