a1f62e4e0170cc618285734a7b992cde6badf686
Agostino Facotti <agostino.facotti@asst-bergamoest.it>
2023-01-27 19:00:39 +0100

    logging: added process PID to default config

--- src/covmatic_robotmanager/__init__.py
+++ src/covmatic_robotmanager/__init__.py
@@ -1,10 +1,9 @@
-from .config import Config
 import logging
 
 __version__ = "0.0.1"
 
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s,%(msecs)03d %(name)-12s %(levelname)-8s: %(message)s',
+    format='%(asctime)s,%(msecs)03d %(process)d %(name)-12s %(levelname)-8s: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
 )
