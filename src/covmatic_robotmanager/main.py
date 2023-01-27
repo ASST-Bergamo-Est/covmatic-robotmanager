49a8ce84c4c242df306735444b7ea1f24dabfdaa
Agostino Facotti <agostino.facotti@asst-bergamoest.it>
2023-01-27 19:04:51 +0100

    Added multiprocessing to shutdown server

--- src/covmatic_robotmanager/main.py
+++ src/covmatic_robotmanager/main.py
@@ -1,12 +1,15 @@
 # RobotManager
 # ============
 # a simple manager to control EVA robot and provide a safe interfate to be used by others.
+import os
 
 from .config import Config
 Config.pull(__doc__)
 
 import logging
 from flask import Flask, request
+
+import multiprocessing
 
 from . import __version__
 from .api import RobotManagerApi
@@ -19,29 +22,33 @@ logger.info("Starting version {}".format(__version__))
 class RobotManagerApp(Flask):
     def __init__(self, name=__name__, *args, **kwargs):
         super(RobotManagerApp, self).__init__(name, *args, **kwargs)
-        RobotManagerApi().init_app(self)
+        self._api = RobotManagerApi()
+        self._api.init_app(self)
 
+    def shutdown(self):
+        self._api.shutdown()
 
-def shutdown_server():
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
+def get_token(q: multiprocessing.Queue) -> None:
+    app = RobotManagerApp()
+
+    @app.route('/shutdown', methods=['POST'])
+    def shutdown():
+        logger.info("Shutting down app...")
+        app.shutdown()
+        logger.info("Releasing process for shutdown")
+        q.put("")
+        return "Shutdown complete"
+
+    app.run(host='::', port=Config().port, debug=False)
 
 
 if __name__ == '__main__':
-    # rm = RobotManager()
-    # rm.
-    app = RobotManagerApp()
-
-    @app.route('/shutdown', methods=['GET'])
-    def shutdown():
-        RobotManagerApi.shutdown()
-        shutdown_server()
-        return "Server is shutting down..."
-
-    app.run(host='::', port=Config().port, debug=True)
-
-
-
-
+    q = multiprocessing.Queue()
+    p = multiprocessing.Process(target=get_token, args=(q,))
+    logger.info("Starting server process...")
+    p.start()
+    logger.info("Server process {} is waiting for shutdown.".format(p.pid))
+    token = q.get(block=True)
+    logger.info("Terminating process {}".format(p.pid))
+    p.terminate()
+    logger.info("Exiting")
